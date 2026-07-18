07dc5be76b8540f583c95bbaab36a5627ecbf2ff
jagadeesh k <k.jagadeesh18@gmail.com>
2026-07-17 21:48:17 -0400

    attributes added in controller

--- ProductCatalog.API/Controllers/productsController.cs
+++ ProductCatalog.API/Controllers/productsController.cs
@@ -1,6 +1,8 @@
 ﻿using Asp.Versioning;
 using Microsoft.AspNetCore.Mvc;
 using ProductCatalog.API.Business.IRepository;
+using ProductCatalog.API.Utility.Exception;
+using ProductCatalog.API.Utility.Model;
 
 namespace ProductCatalog.API.Controllers
 {
@@ -18,6 +20,8 @@ namespace ProductCatalog.API.Controllers
         }
         // GET /api/products
         [HttpGet]
+        [ProducesResponseType(typeof(Product), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetAllProducts()
         {
             try
@@ -34,12 +38,19 @@ namespace ProductCatalog.API.Controllers
 
         // GET /api/products/{id}
         [HttpGet("{id:int}")]
+        [ProducesResponseType(typeof(ProductDetail), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetProductById(int id)
         {
             try
             {
                 var products = await _productService.GetProductByIdAsync(id);
                 return Ok(products);
+            }
+            catch(NotFoundException ex)
+            {
+                _logger.LogWarning(ex, "Product with ID {Id} not found.", id);
+                return NotFound(ex.Message);
             }
             catch (Exception ex)
             {
@@ -49,6 +60,8 @@ namespace ProductCatalog.API.Controllers
         }
         // GET /api/products/metrics
         [HttpGet("metrics")]
+        [ProducesResponseType(typeof(Metrics), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetProductMetrics()
         {
             try
